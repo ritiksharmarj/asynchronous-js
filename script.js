b98cb42c46f87e9eb2299b59ce410d6c14f04e03
ritiksharmarj <theonlyritik@gmail.com>
2023-02-15 01:32:34 +0530

    CallBack Hell

--- script.js
+++ script.js
@@ -5,16 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
-request.send();
-
-request.addEventListener('load', function () {
-   const [data] = JSON.parse(this.responseText);
-   console.log(data);
-
+const renderCountryData = (data, className = '') => {
    const html = `
-        <article class="country">
+        <article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -22,9 +15,11 @@ request.addEventListener('load', function () {
             <p class="country__row"><span>👫</span>${(
                data.population / 1000000
             ).toFixed(1)}M people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(
+               data.languages
+            ).join(', ')}</p>
             <p class="country__row"><span>💰</span>${
-               data.currencies.INR.name
+               Object.values(data.currencies).at(0).name
             }</p>
           </div>
         </article>
@@ -32,4 +27,38 @@ request.addEventListener('load', function () {
 
    countriesContainer.insertAdjacentHTML('beforeend', html);
    countriesContainer.style.opacity = 1;
-});
+};
+
+const getCountryAndNeighbour = (country) => {
+   // AJAX call for country
+   const request = new XMLHttpRequest();
+   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+   request.send();
+
+   request.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+      console.log(data);
+
+      // Render country
+      renderCountryData(data);
+
+      // Get neighbour country
+      const neighbour = data.borders?.[0];
+
+      if (!neighbour) return;
+
+      // AJAX call for neighbour countries
+      const request2 = new XMLHttpRequest();
+      request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+      request2.send();
+
+      request2.addEventListener('load', function () {
+         const [data2] = JSON.parse(this.responseText);
+         console.log(data2);
+
+         renderCountryData(data2, 'neighbour');
+      });
+   });
+};
+
+getCountryAndNeighbour('India');
