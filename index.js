886bb64166f56401739a9670d729194ef49c829d
ritiksharmarj <theonlyritik@gmail.com>
2023-03-09 15:51:23 +0530

    Building Promises

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePromise = (file) => {
+   return new Promise((resolve, reject) => {
+      fs.readFile(file, (err, data) => {
+         if (err) reject('Not found!');
+         resolve(data);
+      });
+   });
+};
+
+const writeFilePromise = (file, data) => {
+   return new Promise((resolve, reject) => {
+      fs.writeFile(file, data, (err) => {
+         if (err) reject('Could not write file 😢');
+         resolve('Success.');
+      });
+   });
+};
+
+readFilePromise(`${__dirname}/dog.txt`)
+   .then((data) => {
+      console.log(`Breed: ${data}`);
+
+      return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+   })
+   .then((res) => {
+      console.log(res.body.message);
+      return writeFilePromise('dog-img.txt', res.body.message);
+   })
+   .then(() => {
+      console.log('Random dog image saved to file!');
+   })
+   .catch((err) => {
+      console.log(err.message);
+   });
+
+/*
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
    console.log(`Breed: ${data}`);
 
@@ -28,3 +64,4 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
          console.log(err.message);
       });
 });
+*/
