235dd811c9c610df30361736f374636359b59bd9
ritiksharmarj <theonlyritik@gmail.com>
2023-03-09 15:25:58 +0530

    Promises - Node.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+   console.log(`Breed: ${data}`);
+
+   superagent
+      .get(`https://dog.ceo/api/breed/${data}/images/random`)
+      // callback
+      // .end((err, res) => {
+      //    // Calling the end function will send the request
+      //    if (err) return console.log(err.message);
+      //    console.log(res.body.message);
+
+      //    fs.writeFile('dog-img.txt', res.body.message, (err) => {
+      //       console.log('Random dog image saved to file!');
+      //    });
+      // });
+
+      // promise with then/catch
+      .then((res) => {
+         console.log(res.body.message);
+         fs.writeFile('dog-img.txt', res.body.message, (err) => {
+            console.log('Random dog image saved to file!');
+         });
+      })
+      .catch((err) => {
+         console.log(err.message);
+      });
+});
